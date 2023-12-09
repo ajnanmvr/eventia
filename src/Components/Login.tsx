62089f2160b3f9fece8b4e490baef0aa0d2e2ec0
ajnanmvr <ajnanpulikkathody@gmail.com>
2023-12-09 17:11:55 +0530

    some errors persist

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Axios from "@/utils/Axios";
 import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 
@@ -7,8 +8,20 @@ export default function Login() {
     <div>
       <GoogleLogin
         onSuccess={(response: any) => {
+          const loggedUser = jwtDecode(response.credential)
           console.log(jwtDecode(response.credential));
           localStorage.setItem('user',JSON.stringify(jwtDecode(response.credential)))
+          
+            Axios.post('/signup' , {
+              ...loggedUser
+            }).then((res)=>{
+              location.reload()
+              console.log(res)
+              const id = res.data._id
+            }).catch((err)=>{
+              console.log(err)
+            })
+        
         }}
         onError={() => {
           console.log("error");
